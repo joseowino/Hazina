deb114b8fb6e918f94f45fc47cfc94e6e6fbdfa8
joseowino <joeoowino@gmail.com>
2025-09-03 15:18:57 +0300

    Add database configuration for SQLite connection

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sqlite' => [
+        'driver' => 'sqlite',
+        'database' => __DIR__ . '/../database/database.sqlite',
+        'prefix' => '',
+        'foreign_key_constraints' => true,
+    ]
+];
